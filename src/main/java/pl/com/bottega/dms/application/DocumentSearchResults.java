24eaddc271e6db6d9bd3a0e82a8067335c149ff3
anna <krzysztoszek.a@gmail.com>
2017-03-05 09:10:38 +0100

    document catalog

--- src/main/java/pl/com/bottega/dms/application/DocumentSearchResults.java
+++ src/main/java/pl/com/bottega/dms/application/DocumentSearchResults.java
@@ -1,7 +1,46 @@
 package pl.com.bottega.dms.application;
 
+import java.util.List;
+
 /**
  * Created by anna on 12.02.2017.
  */
 public class DocumentSearchResults {
+
+    private List<DocumentDto> documents;
+    private Long pagesCount;
+    private Integer pageNumber;
+    private Integer perPage;
+
+    public List<DocumentDto> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<DocumentDto> documents) {
+        this.documents = documents;
+    }
+
+    public Long getPagesCount() {
+        return pagesCount;
+    }
+
+    public void setPagesCount(Long pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public Integer getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
+    }
 }
--- src/main/java/pl/com/bottega/dms/infrastructure/JPADocumentCatalog.java
+++ src/main/java/pl/com/bottega/dms/infrastructure/JPADocumentCatalog.java
@@ -5,12 +5,16 @@ import pl.com.bottega.dms.application.*;
 import pl.com.bottega.dms.model.Confirmation;
 import pl.com.bottega.dms.model.Document;
 import pl.com.bottega.dms.model.DocumentNumber;
+import pl.com.bottega.dms.model.DocumentStatus;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.criteria.*;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class JPADocumentCatalog implements DocumentCatalog {
@@ -20,7 +24,76 @@ public class JPADocumentCatalog implements DocumentCatalog {
 
     @Override
     public DocumentSearchResults find(DocumentQuery documentQuery) {
-        return null;
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Document> criteriaQuery = criteriaBuilder.createQuery(Document.class);
+        CriteriaQuery<Long> countCriteriaQuery = criteriaBuilder.createQuery(Long.class);
+        Root<Document> root = criteriaQuery.from(Document.class);
+        Root<Document> countRoot = countCriteriaQuery.from(Document.class);
+        countCriteriaQuery.select(criteriaBuilder.count(countRoot));
+        String likeExpression = "%" + documentQuery.getPhrase() + "%";
+        root.fetch("confirmations", JoinType.LEFT);
+        Set<Predicate> predicates = createPredicates(documentQuery, criteriaBuilder, root, likeExpression);
+        criteriaQuery.where(predicates.toArray(new Predicate[] {}));
+        countCriteriaQuery.where(predicates.toArray(new Predicate[] {}));
+        Query query = entityManager.createQuery(criteriaQuery);
+        query.setMaxResults(documentQuery.getPerPage());
+        query.setFirstResult((documentQuery.getPageNumber() - 1) * documentQuery.getPerPage());
+        List<Document> documents = query.getResultList();
+        DocumentSearchResults results = new DocumentSearchResults();
+        List<DocumentDto> dtos = new LinkedList<>();
+        for (Document document : documents) {
+            dtos.add(createDocumentDto(document));
+        }
+        results.setDocuments(dtos);
+        results.setPerPage(documentQuery.getPerPage());
+        results.setPageNumber(documentQuery.getPageNumber());
+        Query countQuery = entityManager.createQuery(countCriteriaQuery);
+        Long total = (Long) countQuery.getSingleResult();
+        results.setPagesCount(total / documentQuery.getPerPage() + (total % documentQuery.getPerPage() == 0 ? 0 : 1));
+        return results;
+    }
+
+    private Set<Predicate> createPredicates(DocumentQuery documentQuery, CriteriaBuilder criteriaBuilder, Root<Document> root, String likeExpression) {
+        Set<Predicate> predicates = new HashSet<>();
+        addPhrasePredicate(documentQuery, criteriaBuilder, root, likeExpression, predicates);
+        addStatusPredicate(documentQuery, criteriaBuilder, root, predicates);
+        addCreatorIdPredicate(documentQuery, criteriaBuilder, root, predicates);
+        addCreatedBeforePredicate(documentQuery, criteriaBuilder, root, predicates);
+        addCreatedAfterPredicate(documentQuery, criteriaBuilder, root, predicates);
+        return predicates;
+    }
+
+    private void addCreatedAfterPredicate(DocumentQuery documentQuery, CriteriaBuilder criteriaBuilder, Root<Document> root, Set<Predicate> predicates) {
+        if (documentQuery.getCreatedAfter() != null) {
+            predicates.add(criteriaBuilder.greaterThan(root.get("createdAt"), documentQuery.getCreatedAfter()));
+        }
+    }
+
+    private void addCreatedBeforePredicate(DocumentQuery documentQuery, CriteriaBuilder criteriaBuilder, Root<Document> root, Set<Predicate> predicates) {
+        if (documentQuery.getCreatedBefore() != null) {
+            predicates.add(criteriaBuilder.lessThan(root.get("createdAt"), documentQuery.getCreatedBefore()));
+        }
+    }
+
+    private void addCreatorIdPredicate(DocumentQuery documentQuery, CriteriaBuilder criteriaBuilder, Root<Document> root, Set<Predicate> predicates) {
+        if (documentQuery.getCreatorId() != null) {
+            predicates.add(criteriaBuilder.equal(root.get("creatorId").get("id"), documentQuery.getCreatorId()));
+        }
+    }
+
+    private void addStatusPredicate(DocumentQuery documentQuery, CriteriaBuilder criteriaBuilder, Root<Document> root, Set<Predicate> predicates) {
+        if (documentQuery.getStatus() != null) {
+            predicates.add(criteriaBuilder.equal(root.get("status"), DocumentStatus.valueOf(documentQuery.getStatus())));
+        }
+    }
+
+    private void addPhrasePredicate(DocumentQuery documentQuery, CriteriaBuilder criteriaBuilder, Root<Document> root, String likeExpression, Set<Predicate> predicates) {
+        if (documentQuery.getPhrase() != null)
+            predicates.add(criteriaBuilder.or(
+                criteriaBuilder.like(root.get("title"), likeExpression),
+                criteriaBuilder.like(root.get("content"), likeExpression),
+                criteriaBuilder.like(root.get("number").get("number"), likeExpression)
+        ));
     }
 
     @Override
@@ -28,8 +101,13 @@ public class JPADocumentCatalog implements DocumentCatalog {
         Query query = entityManager.createQuery("FROM Document d LEFT JOIN FETCH d.confirmations WHERE d.number = :nr");
         query.setParameter("nr", documentNumber);
         Document document = (Document) query.getResultList().get(0);
+        DocumentDto documentDto = createDocumentDto(document);
+        return documentDto;
+    }
+
+    private DocumentDto createDocumentDto(Document document) {
         DocumentDto documentDto = new DocumentDto();
-        documentDto.setNumber(documentNumber.getNumber());
+        documentDto.setNumber(document.getNumber().getNumber());
         documentDto.setTitle(document.getTitle());
         documentDto.setContent(document.getContent());
         documentDto.setStatus(document.getStatus().name());
